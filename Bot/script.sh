f89b05223939f64294296528abc2d930f0031e13
Saginbaeff <43472617+Saginbaeff@users.noreply.github.com>
2019-04-09 13:15:48 +0300

    Add files via upload

---
+++ Bot/script.sh
@@ -0,0 +1,17 @@
+sudo apt-get install openssl
+sudo apt-get install libssl-dev
+sudo apt-get install libboost1.65-all-dev
+sudo apt-get install curl
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install git
+sudo apt-get install doxygen
+sudo apt-get install gsasl
+sudo apt-get install libghc-gsasl-dev
+sudo apt-get install gnutls-bin
+sudo apt-get install gnutls-dev
+sudo apt-get install libgnutls-dev
+sudo apt-get install php-mail
+sudo apt-get install sendmail
+
+sudo cmake ${PWD} 
+sudo make   
